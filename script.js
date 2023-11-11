81c04d3c7dd3608091766b58b4e6ddec876c218b
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-11 14:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 function functionName(){
-	console.log("functionName");
+	alert("functionName");
 }
 function AccioJob() {
-	console.log("AccioJob");
+	alert("AccioJob");
 }
 functionName();
 AccioJob();
